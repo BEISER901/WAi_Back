bf0ef953fd38923dde7fa3a310ddca29dcf6d499
Nikita Bondar <75673468+BEISER901@users.noreply.github.com>
2024-08-09 20:46:16 +0500

    Update start.js

--- start.js
+++ start.js
@@ -4,22 +4,30 @@
 
 const Client = require("./src/Client.js")
 const qrcode = require('qrcode-terminal')
-const client = new Client()
 
-client.id = null
+const startClient = async (clientId) => {
+    const client = new Client()
 
-client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-	qrcode.generate(qr, {small: true})
-});
-
-client.on('ready', async () => {
-    const chats = await client.getChats()
-    // chats[index] index is where chat you have get, index - 0 is first chat in your list.
-    await client.openChat(chats[0].name)
-    console.log(await client.getCurrentChatName())
-    console.log(await client.getAllMessages())
-});
+    // If clientId undefined or null will create new clientId.
+    client.id = clientId
 
 
-client.initialize();
+
+    client.on('qr', (qr) => {
+        // Generate and scan this code with your phone
+        qrcode.generate(qr, {small: true})
+    });
+
+    client.on('ready', async () => {
+        const chats = await client.getChats()
+        // chats[index] index is where chat you have get, index - 0 is first chat in your list.
+        await client.openChat(chats[0].name)
+        console.log(await     client.getCurrentChatName())
+        console.log(await client.getAllMessages())
+    });
+
+
+    client.initialize();
+}
+
+startClient()
