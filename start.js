ef6ad5495063bed357204528c00a3758c0a0c6e3
Nikita Bondar <75673468+BEISER901@users.noreply.github.com>
2024-08-09 20:04:42 +0500

    Update start.js

--- start.js
+++ start.js
@@ -13,16 +13,6 @@ client.on('qr', (qr) => {
 	qrcode.generate(qr, {small: true})
 });
 
-function compare( a, b ) {
-  if ( a.rowId < b.rowId ){
-    return 1;
-  }
-  if ( a.rowId > b.rowId ){
-    return -1;
-  }
-  return 0;
-}
-
 client.on('ready', async () => {
     const chats = await client.getChats()
     // chats[index] index is where chat you have get, index - 0 is first chat in your list.
